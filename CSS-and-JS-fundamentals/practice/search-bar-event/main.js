9ec8fea02e5e4f8717c2d3294fe3ba46f2f570c2
Yevhen Dovhan <y.d.devbox@gmail.com>
2020-09-28 21:13:39 +0300

    Searching bar app done

--- CSS-and-JS-fundamentals/practice/search-bar-event/main.js
+++ CSS-and-JS-fundamentals/practice/search-bar-event/main.js
@@ -1,1 +1,18 @@
-console.log('Hey, it`s working!');
+const searchInput = document.getElementById('searchInput');
+
+function typingHandeler(event) {
+  let searchQuery = event.target.value.toLowerCase();
+  let names = document.getElementsByClassName('name');
+  
+  for (let counter = 0; counter < names.length; counter++) {
+    const currentName = names[counter].textContent.toLowerCase();
+    if (currentName.includes(searchQuery)) {
+      names[counter].style.display = "block";
+    } else {
+      names[counter].style.display = "none";
+    }
+  }
+}
+
+searchInput.addEventListener('keyup', typingHandeler);
+
