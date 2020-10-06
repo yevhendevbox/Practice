46c2bbc15aac8e7e3e80c9591a67e9b1d2707c19
Yevhen Dovhan <y.d.devbox@gmail.com>
2020-10-06 20:55:01 +0300

    Move snake logic added

--- CSS-and-JS-fundamentals/practice/snake-game/js/main.js
+++ CSS-and-JS-fundamentals/practice/snake-game/js/main.js
@@ -2,7 +2,9 @@ const grid = document.querySelector('.grid');
 const startBtn = document.querySelector('#start');
 const score = document.querySelector('#score');
 let squares = [];
-let curentSnake = [0,1,2];
+let currentSnake = [2,1,0];
+let direction = 1;
+let width = 10;
 
 function createGrid(){
   for (let i = 0; i < 100; i++) {
@@ -15,4 +17,30 @@ function createGrid(){
 
 createGrid();
 
-curentSnake.forEach(index => squares[index].classList.add('snake'));
+currentSnake.forEach(index => squares[index].classList.add('snake'));
+
+function move(){
+  const tail = currentSnake.pop();
+  squares[tail].classList.remove('snake');
+  currentSnake.unshift(currentSnake[0] + direction);
+  squares[currentSnake[0]].classList.add('snake');
+}
+
+move();
+
+const timerID = setInterval(move, 1000);
+// clearInterval(timerID);
+
+function control(e){
+  if(e.keyCode === 39){
+    direction = 1;
+  } else if(e.keyCode === 38){
+    direction = -width;
+  } else if(e.keyCode === 37){
+    direction = -1;
+  } else if(e.keyCode === 40){
+    direction = +width;
+  }
+}
+
+document.addEventListener('keyup', control);
