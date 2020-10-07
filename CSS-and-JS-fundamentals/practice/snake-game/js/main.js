648c337a2c44a25da2fd50f9348af0da26ccfb12
Yevhen Dovhan <y.d.devbox@gmail.com>
2020-10-07 22:41:26 +0300

    Added wall hit logic, added apple generator

--- CSS-and-JS-fundamentals/practice/snake-game/js/main.js
+++ CSS-and-JS-fundamentals/practice/snake-game/js/main.js
@@ -4,10 +4,12 @@ const score = document.querySelector('#score');
 let squares = [];
 let currentSnake = [2,1,0];
 let direction = 1;
-let width = 10;
+const width = 10;
+let appleIndex = 0;
 
 function createGrid(){
-  for (let i = 0; i < 100; i++) {
+  // creating grid for the game
+  for (let i = 0; i < width*width; i++) {
     const newElement = document.createElement('div');
     grid.appendChild(newElement);
     newElement.classList.add('square');
@@ -17,20 +19,40 @@ function createGrid(){
 
 createGrid();
 
+// Making actual snake on the grid
 currentSnake.forEach(index => squares[index].classList.add('snake'));
 
 function move(){
+  // Snake hit the walls logic
+  if(
+    (currentSnake[0] + width >= width*width && direction === width) ||
+    (currentSnake[0] % width === width-1 && direction === 1) ||
+    (currentSnake[0] % width === 0 && direction === -1) ||
+    (currentSnake[0] - width < 0 && direction === -width) ||
+    snake[currentSnake[0] + direction].classList.contains('snake')
+    )
+
+  return clearInterval(timerID);
+  // Make snake to move
   const tail = currentSnake.pop();
   squares[tail].classList.remove('snake');
   currentSnake.unshift(currentSnake[0] + direction);
   squares[currentSnake[0]].classList.add('snake');
 }
-
 move();
-
 const timerID = setInterval(move, 1000);
-// clearInterval(timerID);
 
+// Generate apples
+function generateApples(){
+  do{
+    appleIndex = Math.floor(Math.random()* squares.length)
+  } while (squares[appleIndex].classList.contains('snake')) {
+    squares[appleIndex].classList.add('apple');
+  }
+}
+generateApples();
+
+// Controling the direction of the snake
 function control(e){
   if(e.keyCode === 39){
     direction = 1;
@@ -43,4 +65,5 @@ function control(e){
   }
 }
 
+// event for arrows to press
 document.addEventListener('keyup', control);
