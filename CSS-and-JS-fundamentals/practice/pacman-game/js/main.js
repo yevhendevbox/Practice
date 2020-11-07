27eec882ab2edb5103b949317fb85f9c50530b9d
Yevhen Dovhan <y.d.devbox@gmail.com>
2020-11-07 23:36:37 +0200

    Finished basic pacman game

---
+++ CSS-and-JS-fundamentals/practice/pacman-game/js/main.js
@@ -0,0 +1,217 @@
+//28 * 28 = 784
+  // 0 - pac-dots
+  // 1 - wall
+  // 2 - ghost-lair
+  // 3 - power-pellet
+  // 4 - empty
+const width = 28;
+const grid = document.querySelector('.grid');
+const scoreDisplay = document.querySelector('#score');
+let squares = [];
+let score = 0;
+
+
+// ///////////////////////////////////////////////////////////////////
+// Board layout
+const layout = [
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,3,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,3,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,4,4,4,4,4,4,4,4,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,2,2,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+    4,4,4,4,4,4,0,0,0,4,1,2,2,2,2,2,2,1,4,0,0,0,4,4,4,4,4,4,
+    1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,0,0,0,0,0,0,0,0,4,4,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,3,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,3,1,
+    1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+    1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+    1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+    1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+];
+
+function createBoard() {
+  for (let i = 0; i < layout.length; i++) {
+    const square = document.createElement('div');
+    grid.appendChild(square);
+    squares.push(square);
+
+    if (layout[i] === 0) {
+      squares[i].classList.add('pac-dot')
+    } else if (layout[i] === 1) {
+      squares[i].classList.add('wall')
+    } else if (layout[i] === 2) {
+      squares[i].classList.add('ghost-lair')
+    } else if (layout[i] === 3) {
+      squares[i].classList.add('power-pellet')
+    }
+  }
+}
+createBoard();
+
+// Pacman position
+// ///////////////////////////////////////////////////////////////////////////////////////////////////
+let pacmanCurrentIndex = 490;
+squares[pacmanCurrentIndex].classList.add('pacman');
+
+// Moving logic
+// //////////////////////////////////////////////////////////////////////////////////////////////////
+function control(e) {
+  squares[pacmanCurrentIndex].classList.remove('pacman');
+  switch (e.keyCode) {
+    case 40:
+      if (!squares[pacmanCurrentIndex + width].classList.contains('wall') &&
+        !squares[pacmanCurrentIndex + width].classList.contains('ghost-lair') &&
+        pacmanCurrentIndex + width < width * width) {
+        pacmanCurrentIndex += width;
+      }
+      break;
+    case 38:
+      if (!squares[pacmanCurrentIndex - width].classList.contains('wall') &&
+        !squares[pacmanCurrentIndex - width].classList.contains('ghost-lair') &&
+        pacmanCurrentIndex - width >= 0) {
+        pacmanCurrentIndex -= width;
+      }
+      break;
+    case 37:
+      if (!squares[pacmanCurrentIndex - 1].classList.contains('wall') &&
+        !squares[pacmanCurrentIndex - 1].classList.contains('ghost-lair') &&
+        pacmanCurrentIndex % width !== 0) {
+        pacmanCurrentIndex -= 1;
+        if (pacmanCurrentIndex === 364) {
+          pacmanCurrentIndex = 391
+        }
+      }
+      break;
+    case 39:
+      if (!squares[pacmanCurrentIndex + 1].classList.contains('wall') &&
+        !squares[pacmanCurrentIndex + 1].classList.contains('ghost-lair') &&
+        pacmanCurrentIndex % width < width - 1) {
+        pacmanCurrentIndex += 1;
+        if (pacmanCurrentIndex === 391) {
+          pacmanCurrentIndex = 364
+        }
+      }
+      break;
+  }
+  squares[pacmanCurrentIndex].classList.add('pacman');
+  pacDotEaten();
+  powerPelletEaten();
+  checkForWin();
+  checkForGameOver();
+}
+document.addEventListener('keyup', control);
+
+// Pacman eating dots logic 
+// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+function pacDotEaten() {
+  if(squares[pacmanCurrentIndex].classList.contains('pac-dot')) {
+    squares[pacmanCurrentIndex].classList.remove('pac-dot');
+    score++;
+    scoreDisplay.textContent = score;
+  }
+}
+
+// Eating Power Pellet logic goes here
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function powerPelletEaten(){
+  if(squares[pacmanCurrentIndex].classList.contains('power-pellet')){
+    squares[pacmanCurrentIndex].classList.remove('power-pellet');
+    score+=10;
+    ghosts.forEach(ghost => ghost.isScared = true);
+    setTimeout(unScareGhosts, 10000);
+  }
+}
+
+function unScareGhosts(){
+  ghosts.forEach(ghost => ghost.isScared = false);
+}
+// Creating a Ghost
+// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class Ghost{
+  constructor(className, startIndex, speed){
+    this.className = className;
+    this.startIndex = startIndex;
+    this.speed = speed;
+    this.currentIndex = startIndex;
+    this.isScared = false;
+    this.timerId = NaN;
+  }
+} 
+const ghosts = [
+  new Ghost('blinky', 348, 250),
+  new Ghost('pinky', 376, 400),
+  new Ghost('inky', 351, 351, 300),
+  new Ghost('clyde', 379, 500)
+]
+ghosts.forEach(ghost => squares[ghost.currentIndex].classList.add(ghost.className, 'ghost'));
+
+// Ghost moving logic
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+ghosts.forEach(ghost => moveGhost(ghost));
+
+function moveGhost(ghost) {
+  const directions = [-1, +1, -width, +width];
+  let direction = directions[Math.floor(Math.random() * directions.length)];
+
+  ghost.timerId = setInterval(function () {
+    if (!squares[ghost.currentIndex + direction].classList.contains('wall') && !squares[ghost.currentIndex + direction].classList.contains('ghost')) {
+      squares[ghost.currentIndex].classList.remove(ghost.className, "ghost", "scared-ghost");
+      ghost.currentIndex += direction;
+      squares[ghost.currentIndex].classList.add(ghost.className, "ghost");
+    } else {
+      direction = directions[Math.floor(Math.random() * directions.length)];
+    }
+    if (ghost.isScared) {
+      squares[ghost.currentIndex].classList.add('scared-ghost');
+    }
+    // Pacman eating ghosts logic 
+    // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    if (ghost.isScared && squares[ghost.currentIndex].classList.contains('pacman')) {
+      squares[ghost.currentIndex].classList.remove(ghost.className, 'ghost', 'scared-ghost');
+      ghost.currentIndex = ghost.startIndex;
+      score += 100;
+      squares[ghost.currentIndex].classList.add(ghost.className, 'ghost');
+    }
+    checkForGameOver();
+  }, ghost.speed);
+};
+
+// Check for Game Over
+// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function checkForGameOver(){
+  if(squares[pacmanCurrentIndex].classList.contains('ghost') && !squares[pacmanCurrentIndex].classList.contains('scared-ghost')){
+    ghosts.forEach(ghost => clearInterval(ghost.timerId));
+    document.removeEventListener('keyup', control);
+    scoreDisplay.innerHTML = 'You LOSE!';
+  }
+}
+
+// Check for Win
+// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function checkForWin(){
+  if(score === 274) {
+    ghosts.forEach(ghost => clearInterval(ghost.timerId));
+    document.removeEventListener('keyup', control);
+    scoreDisplay.innerHTML = 'You WON!';
+  }
+}
--- CSS-and-JS-fundamentals/practice/search-bar-event/main.js
+++ CSS-and-JS-fundamentals/practice/search-bar-event/main.js
@@ -16,3 +16,121 @@ function typingHandeler(event) {
 
 searchInput.addEventListener('keyup', typingHandeler);
 
+
+
+// Creating DATA using fetch API
+
+
+// fetch('https://jsonplaceholder.typicode.com/posts', {
+//   method: 'POST',
+//   body: JSON.stringify({
+//     title: 'foo',
+//     body: 'bar',
+//     userId: 1,
+//   }),
+//   headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//   },
+// })
+// .then((response) => response.json())
+// .then((json) => console.log(json))
+
+
+// let urlAPI = " https://sv443.net/jokeapi/v2";
+// const apiUrl = "https://api.chucknorris.io/jokes/random";
+
+// async function getNewJoke() {
+//   const response = await fetch(apiUrl);
+//   const joke = await response.json();
+
+//   console.log(joke.value);
+// }
+
+// getNewJoke();
+
+// function arrayReplace(array, elemToReplace, substitutionElem) {
+//   //  write code here.
+//   for ( let i = 0; i < array.length; i++) {
+//       if ( array[i] === elemToReplace) {
+//           array[i] = substitutionElem;
+//       }
+//   }
+//   return array;
+// }
+
+// arrayReplace([1, 2, 1], 1, 3);
+
+
+// function caseInsensitivePalindrome(str) {
+//   //  write code here.
+//   let reverseStr = str.split('').reverse();
+//   return reverseStr.join('').toLowerCase() === str.toLowerCase();
+// }
+// caseInsensitivePalindrome('AaBac');
+
+// function encloseInBrackets(str) {
+//   //  write code here.
+//   const newStr = '(' + str + ')';
+//   return newStr;
+// }
+
+// encloseInBrackets('ananana');
+
+// function factorialNumber(num) {
+//   // write code here
+//   if (num === 0 || num === 1) {
+//     return 1;
+//   }
+//   for (let i = num - 1; i >= 1; i--) {
+//     num = num * i;
+//   }
+//   console.log(num);
+//   return num;
+// }
+// factorialNumber(5);
+
+// function firstDigit(str) {
+//   //  write code here.
+//   let strToArr = str.split('');
+//   const examArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+//   for ( let char of strToArr) {
+//     if(examArr.includes(char)) {
+//       console.log(char);
+//       return char;
+//     }
+//   }
+
+// }
+
+// firstDigit("var_1__Int2");
+
+// function largestNumber(num) {
+//   //  write code here.
+//   let container = '';
+
+//   for (let i = 0; i < num; i++) {
+//     container = container.concat('9');
+//   }
+//   return parseInt(container);
+// }
+// console.log(largestNumber(5));
+// largestNumber(5);
+
+// function maxMultiple(divisor, bound) {
+//   //  write code here.
+//   for (let i = 1; i < bound; i++) {
+//     let multipleValue = divisor * i;
+//     if (multipleValue === bound - 1) {
+//       return multipleValue;
+//     }
+//   }
+// }
+// console.log(maxMultiple(3, 10));
+
+function maxMultiple(divisor, bound) {
+  //  write code here.
+  const reminder = bound % divisor;
+
+  return bound - reminder;
+}
